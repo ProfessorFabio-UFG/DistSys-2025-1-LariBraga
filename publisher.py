260a22fde20c08a11a8cbd87c897562a176e0f00
LariBraga <163949909+LariBraga@users.noreply.github.com>
2025-05-18 14:24:57 -0300

    resp

--- publisher.py
+++ publisher.py
@@ -1,5 +1,24 @@
 import zmq, time
+import random
 from constPS import * #-
+
+def calcula(): # realiza uma conta aleátoria de soma, subtração ou multiplicação
+	op = random.randint(0,2)
+	num1 = random.randint(0,20)
+	num2 = random.randint(0,20)
+
+	if op == 0:
+		op_sinal = " + "
+		res = num1 + num2
+	elif op == 1:
+		op_sinal = " - "
+		res = num1 - num2
+	else:
+		op_sinal = " * "
+		res = num1 * num2
+	
+	return str(num1) + op_sinal + str(num2) + " = " + str(res)
+
 
 context = zmq.Context()
 s = context.socket(zmq.PUB)        # create a publisher socket
@@ -7,5 +26,7 @@ p = "tcp://"+HOST+":"+ PORT      # how and where to communicate
 s.bind(p)                          # bind socket to the address
 while True:
 	time.sleep(5)                    # wait every 5 seconds
-	msg = str.encode("TIME " + time.asctime())
-	s.send(msg) # publish the current time
+	msg1 = str.encode("TIME " + time.asctime())
+	s.send(msg1) # publish the current time
+	msg2 = str.encode("CONTA " + calcula())
+	s.send(msg2) # publish a calculation of sum, subtration or multiplication
--- subscriber.py
+++ subscriber.py
@@ -6,7 +6,11 @@ s = context.socket(zmq.SUB)          # create a subscriber socket
 p = "tcp://"+ HOST +":"+ PORT        # how and where to communicate
 s.connect(p)                         # connect to the server
 s.setsockopt_string(zmq.SUBSCRIBE, "TIME")  # subscribe to TIME messages
+s.setsockopt_string(zmq.SUBSCRIBE, "CONTA") # subscribe to CONTA messages
 
 for i in range(5):  # Five iterations
-	time = s.recv()   # receive a message
+	time = s.recv()   # receive a TIME's message
 	print (bytes.decode(time))
+	conta = s.recv() # receive a CONTA's message
+	print(bytes.decode(conta))
+	print("")
